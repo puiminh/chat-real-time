a493044fc498cd99eebf206ded2f4ea96a6a386b
Bùi Minh <volmicrolab@gmail.com>
2022-12-22 07:57:44 +0000

    upload img

--- public/app.js
+++ public/app.js
@@ -100,3 +100,110 @@ function changeRoom(room) {
     document.getElementById(currentRoom).classList.add("active_item");
   }
 }
+
+
+
+//Tai anh len:
+
+var file;
+var hasImg = false;
+
+document.getElementById("file").addEventListener('change', (e) => {
+	console.log(event);
+	file = event.target.files[0];
+	console.log(file);
+    output.src = URL.createObjectURL(event.target.files[0]);
+	output.style.display = "block";
+    output.onload = function() {
+      URL.revokeObjectURL(output.src) // free memory
+    }
+
+    hasImg = true;
+
+    uploadToFileBase();
+})
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-storage.js";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+    apiKey: "AIzaSyCenHuhDER2MN5S5jt8j7DMWaPBQcYFQAw",
+    authDomain: "uploadfiletofirebase-ae63f.firebaseapp.com",
+    projectId: "uploadfiletofirebase-ae63f",
+    storageBucket: "uploadfiletofirebase-ae63f.appspot.com",
+    messagingSenderId: "548678075768",
+    appId: "1:548678075768:web:d9a471afe85756f666348d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Storage and get a reference to the service
+const storage = getStorage(app);
+
+function uploadToFileBase() {
+      const metadata = {
+      contentType: 'image/jpeg'
+    };
+    const storage = getStorage();
+    const storageRef = ref(storage, 'images/' + file.name);
+    const uploadTask = uploadBytesResumable(storageRef, file, metadata);
+  //       uploadBytes(storageRef, file).then((snapshot) => {
+  //     console.log('Uploaded a blob or file!');
+  // });
+
+  uploadTask.on('state_changed',
+  (snapshot) => {
+  // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+  const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+  console.log('Upload is ' + progress + '% done');
+  switch (snapshot.state) {
+    case 'paused':
+    console.log('Upload is paused');
+    break;
+    case 'running':
+    console.log('Upload is running');
+    break;
+  }
+  }, 
+  (error) => {
+  // A full list of error codes is available at
+  // https://firebase.google.com/docs/storage/web/handle-errors
+  switch (error.code) {
+    case 'storage/unauthorized':
+    // User doesn't have permission to access the object
+    break;
+    case 'storage/canceled':
+    // User canceled the upload
+    break;
+
+    // ...
+
+    case 'storage/unknown':
+    // Unknown error occurred, inspect error.serverResponse
+    break;
+  }
+  }, 
+  () => {
+  // Upload completed successfully, now we can get the download URL
+  getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+    console.log('File available at', downloadURL);
+    hasImg = false;
+    output.style.display = "none";
+    
+    // sendMsg(downloadURL);
+    msgText.value = "";
+    msgText.focus();
+    // chatBox.scrollTop = chatBox.scrollHeight;
+  });
+  }
+  );
+
+}
+
+function checkURL(url) {
+  return url?.includes("uploadfiletofirebase-ae63f.appspot.com");
+}
